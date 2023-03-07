d0d8bdb765ded055b141df1e68a4b6432c4ca758
totarochristian <totaro.christian.18@gmail.com>
2023-03-07 15:17:12 +0100

    Add the const definition of the fixed number to add at the end of the password

--- js/my-script.js
+++ js/my-script.js
@@ -1,3 +1,4 @@
 let userName = prompt("Inserisci il tuo nome");
 let userLastName = prompt("Inserisci il tuo cognome");
 let userColor = prompt("Inserisci il tuo colore preferito");
+const fixedNumber = 21;
