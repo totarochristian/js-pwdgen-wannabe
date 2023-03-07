dad715ce2324cab3813d95f7f9f93f81a49f10c6
totarochristian <totaro.christian.18@gmail.com>
2023-03-07 15:46:59 +0100

    Add controls to check if the inserted values are null or empty and in that case, set a default value.

--- js/my-script.js
+++ js/my-script.js
@@ -2,5 +2,14 @@ let userName = prompt("Inserisci il tuo nome");
 let userLastName = prompt("Inserisci il tuo cognome");
 let userColor = prompt("Inserisci il tuo colore preferito");
 const fixedNumber = 21;
+if (!userName || userName.length === 0){
+    userName = "nome";
+}
+if (!userLastName || userLastName.length === 0){
+    userLastName = "cognome";
+}
+if (!userColor || userColor.length === 0){
+    userColor = "colorepreferito";
+}
 let password = userName + userLastName + userColor + fixedNumber;
 document.getElementById("GeneratedPassword").innerHTML = password;
