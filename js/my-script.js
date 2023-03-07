4620a4ce89d86011720045bf95fb31875460b1f5
totarochristian <totaro.christian.18@gmail.com>
2023-03-07 15:50:50 +0100

    Add alert message to each controls to notify the user if the value insert is wrong

--- js/my-script.js
+++ js/my-script.js
@@ -3,12 +3,15 @@ let userLastName = prompt("Inserisci il tuo cognome");
 let userColor = prompt("Inserisci il tuo colore preferito");
 const fixedNumber = 21;
 if (!userName || userName.length === 0 || userName === ' '){
+    alert("Il nome inserito non è valido");
     userName = "nome";
 }
 if (!userLastName || userLastName.length === 0 || userLastName === ' '){
+    alert("Il cognome inserito non è valido");
     userLastName = "cognome";
 }
 if (!userColor || userColor.length === 0 || userColor === ' '){
+    alert("Il colore inserito non è valido");
     userColor = "colorepreferito";
 }
 let password = userName + userLastName + userColor + fixedNumber;
