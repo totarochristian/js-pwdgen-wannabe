d7e03ff68a074801ac68688d66588bf627824fdf
totarochristian <totaro.christian.18@gmail.com>
2023-03-07 15:19:00 +0100

    Add the console log function to test the generated password.

--- js/my-script.js
+++ js/my-script.js
@@ -3,3 +3,4 @@ let userLastName = prompt("Inserisci il tuo cognome");
 let userColor = prompt("Inserisci il tuo colore preferito");
 const fixedNumber = 21;
 let password = userName + userLastName + userColor + fixedNumber;
+console.log(password);
