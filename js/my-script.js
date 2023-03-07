a65a094674285c045f49e761c90722ced1a04326
totarochristian <totaro.christian.18@gmail.com>
2023-03-07 15:48:39 +0100

    Add condition to check if the variables inserted by the user have an empty space

--- js/my-script.js
+++ js/my-script.js
@@ -2,13 +2,13 @@ let userName = prompt("Inserisci il tuo nome");
 let userLastName = prompt("Inserisci il tuo cognome");
 let userColor = prompt("Inserisci il tuo colore preferito");
 const fixedNumber = 21;
-if (!userName || userName.length === 0){
+if (!userName || userName.length === 0 || userName === ' '){
     userName = "nome";
 }
-if (!userLastName || userLastName.length === 0){
+if (!userLastName || userLastName.length === 0 || userLastName === ' '){
     userLastName = "cognome";
 }
-if (!userColor || userColor.length === 0){
+if (!userColor || userColor.length === 0 || userColor === ' '){
     userColor = "colorepreferito";
 }
 let password = userName + userLastName + userColor + fixedNumber;
