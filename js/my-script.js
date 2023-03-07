b0ceb94cf1555add3aa31a23b287f52f7e22526f
totarochristian <totaro.christian.18@gmail.com>
2023-03-07 15:54:02 +0100

    Refactoring of the code

--- js/my-script.js
+++ js/my-script.js
@@ -1,18 +1,27 @@
+const fixedNumber = 21;//Fixed number to add at the end of the generated password
+
+//Ask the name of the user and check the value
 let userName = prompt("Inserisci il tuo nome");
-let userLastName = prompt("Inserisci il tuo cognome");
-let userColor = prompt("Inserisci il tuo colore preferito");
-const fixedNumber = 21;
 if (!userName || userName.length === 0 || userName === ' '){
     alert("Il nome inserito non è valido");
     userName = "nome";
 }
+
+//Ask the name of the user and check the value
+let userLastName = prompt("Inserisci il tuo cognome");
 if (!userLastName || userLastName.length === 0 || userLastName === ' '){
     alert("Il cognome inserito non è valido");
     userLastName = "cognome";
 }
+
+//Ask the favorite color of the user and check the value
+let userColor = prompt("Inserisci il tuo colore preferito");
 if (!userColor || userColor.length === 0 || userColor === ' '){
     alert("Il colore inserito non è valido");
     userColor = "colorepreferito";
 }
+
+//Generate the password concatenating the variables inserted by the user
 let password = userName + userLastName + userColor + fixedNumber;
+//Define the password generated as the text of the paragraph with the id "GeneratedPassword"
 document.getElementById("GeneratedPassword").innerHTML = password;
